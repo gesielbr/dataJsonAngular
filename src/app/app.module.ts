5dc3cc5399cb984ca48fd67a3cc9373621d42728
Gesiel Oliveira <gesiel.oliveira@linx.com.br>
2022-07-27 13:24:33 -0300

    NgFor Json, Interface, responsive table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentlistComponent } from './studentlist/studentlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentlistComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/studentlist/studentlist.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import studentData from '../student.json';
+
+interface Student {
+  id: Number;
+  name: String;
+  email: String;
+  gender: String;
+}
+
+@Component({
+  selector: 'app-studentlist',
+  templateUrl: './studentlist.component.html',
+  styleUrls: ['./studentlist.component.css'],
+})
+export class StudentlistComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  students: Student[] = studentData;
+}
